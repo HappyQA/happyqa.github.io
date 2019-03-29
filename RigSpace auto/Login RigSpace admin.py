5c6339948f178899deed6d795972c45efce92deb
happyqa <fh.2007@mail.ru>
2019-03-29 12:25:24 +0300

    Run test addeed

--- RigSpace auto/Login RigSpace admin.py
+++ RigSpace auto/Login RigSpace admin.py
@@ -11,8 +11,8 @@ class LoginRIGSpace(unittest.TestCase):
     def test_login_rigspace(self):
         driver = self.driver
         # Open rig_space on standalone and clear the cookies
-        driver.get("http://standalone.rig.space/admin/manage/users")
         driver.delete_all_cookies()
+        driver.get("http://standalone.rig.space/admin/")
         assert "Rigspace Login" in driver.title
         # Enter username
         driver.find_element_by_name("username").send_keys("admin")
@@ -20,6 +20,7 @@ class LoginRIGSpace(unittest.TestCase):
         driver.find_element_by_name("password").send_keys("1")
         # Click on button "Submit"
         driver.find_element_by_css_selector("button[type]").send_keys(Keys.RETURN)
+        print(driver.get_cookies())
 
     def tearDown(self):
         self.driver.close()
---
+++ RigSpace auto/Login Rigspace equipment.py
@@ -0,0 +1,28 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class LoginRIGSpace(unittest.TestCase):
+    def setUp(self):
+        # Choose browser if needed (IE,Chrome)
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+
+    def test_login_rigspace(self):
+        driver = self.driver
+        # Open rig_space on standalone and clear the cookies
+        driver.get("http://standalone.rig.space/equipment/")
+        driver.delete_all_cookies()
+        assert "Rigspace Login" in driver.title
+        # Enter username
+        driver.find_element_by_name("username").send_keys("admin")
+        # Enter password
+        driver.find_element_by_name("password").send_keys("1")
+        # Click on button "Submit"
+        driver.find_element_by_css_selector("button[type]").send_keys(Keys.RETURN)
+
+    def tearDown(self):
+        self.driver.close()
+
+    if __name__ == "__main__":
+        unittest.main()
