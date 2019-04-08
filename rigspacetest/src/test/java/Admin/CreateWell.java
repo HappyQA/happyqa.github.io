4de6938475d6cd7ec0031f2198c368731ef01e19
happyqa <fh.2007@mail.ru>
2019-04-08 17:07:06 +0300

    createnewutilites

--- rigspacetest/src/test/java/Admin/CreateWell.java
+++ rigspacetest/src/test/java/Admin/CreateWell.java
@@ -1,5 +1,6 @@
 package Admin;
 import static utilites.AuthorizationPage.*;
+import com.codeborne.selenide.Selenide;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
@@ -26,6 +27,7 @@ public class CreateWell {
         selectContains("Недропользователь", "Тестовые");
         selectContains("Назначение", "Тестовые");
         $(By.xpath("//*[@id=\"main-content\"]/div/app-wells/app-nav-root/div/app-well/app-well-card/div/app-well-form/form/div[2]/button")).click();
+        Selenide.screenshot("1.png");
     }
     @After
     public void tearDown () throws Exception {
