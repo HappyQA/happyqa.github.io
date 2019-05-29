cba81f503829d35e57ec81430cc77cdcc57dbaf5
a.stupin <a.stupin@tetra-soft.ru>
2019-05-29 18:49:39 +0300

    update project

--- rigspacetest/src/test/java/utilites/testURL.java
+++ rigspacetest/src/test/java/utilites/testURL.java
@@ -1,18 +1,19 @@
 package utilites;
+import org.junit.Before;
 import static com.codeborne.selenide.Selenide.open;
 
 public class testURL {
+
     public static void openReportingMainPage() {
-        open("http://testing.rig.space/reporting/");
+        open("http://dev.rig.space/reporting/");
     }
     public static void openAdminMainPage() {
-
-        open("http://testing.rig.space/admin/");
+        open("http://dev.rig.space/admin/");
     }
     public static void openEquipmentMainPage() {
-        open("http://testing.rig.space/equipment/");
+        open("http://dev.rig.space/equipment/");
     }
     public static void openAnalyticMainPage () {
-        open("http://testing.rig.space/analytics/");
+        open("http://dev.rig.space/analytics/");
     }
 }
