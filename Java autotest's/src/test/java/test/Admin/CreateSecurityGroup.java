06acc6da045bdc582cda03efcfdb8faf64a7ef0a
a.stupin <fh.2007@mail.ru>
2019-07-08 18:21:49 +0300

    Added new class/ButtonPlus and refactoring class CreateSG

--- Java autotest's/src/test/java/test/Admin/CreateSecurityGroup.java
+++ Java autotest's/src/test/java/test/Admin/CreateSecurityGroup.java
@@ -1,11 +1,12 @@
 package test.Admin;
 import com.codeborne.selenide.Condition;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.*;
 import static utilites.AuthorizationPage.login;
+import static utilites.ButtonPlus.appGroupObject;
+import static utilites.ButtonPlus.appGroupUsers;
 import static utilites.testURL.*;
 
 /**
@@ -26,7 +27,18 @@ public class CreateSecurityGroup {
         $(By.xpath("//*[@id=\"groups\"]/div/div[2]/a")).click();
         $(By.xpath("//input[contains(@placeholder, 'Название')]")).sendKeys("Group for Auto User");
         $(By.xpath("//span[contains(text(), 'Участники')]")).click();
-        $(By.xpath("//*[@id=\"main-content\"]/div/app-manage/app-nav-root/div/app-group/app-group-card/div/div/app-group-users/div/a")).click();
-        $(By.xpath("//*[@id=\"mat-checkbox-7-input\"]")).click();
+        //Push button plus
+        appGroupUsers();
+        $(By.xpath("//*[@id=\"mat-checkbox-2\"]/label")).click();
+        $(By.xpath("//span[contains(text(), 'Добавить (1)')]")).click();
+        //Checking if this user are really added in this group (Admin)
+        $(By.xpath("//*[@id=\"users\"]/div/div[1]/mat-table/mat-row/mat-cell[1]/div")).shouldBe(Condition.visible).shouldHave(Condition.text("Админ"));
+
+        //Adding all object for this user
+        $(By.xpath("//span[contains(text(), 'Объекты')]")).click();
+        //Push button plus
+        appGroupObject();
+        $(By.xpath("//mat-label[contains(text(), 'Тип объекта')]")).click();
+
     }
 }
---
+++ Java autotest's/src/test/java/utilites/ButtonPlus.java
@@ -0,0 +1,15 @@
+package utilites;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ButtonPlus {
+    public static void  appGroupUsers () {
+        $(By.xpath("//*[@id=\"main-content\"]/div/app-manage/app-nav-root/div/app-group/app-group-card/div/div/app-group-users/div/a")).click();
+    }
+    public static void  appGroupObject () {
+        $(By.xpath("//*[@id=\"main-content\"]/div/app-manage/app-nav-root/div/app-group/app-group-card/div/div/app-group-objects/div/a")).click();
+    }
+    public static void appGroupRole () {
+        $(By.xpath("//*[@id=\"main-content\"]/div/app-manage/app-nav-root/div/app-group/app-group-card/div/div/app-group-role/div/a")).click();
+    }
+}
